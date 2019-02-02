9cdf3d9a3f7f1138a8bec0e4da82d1704132a321
User <user@Users-MacBook-Pro.local>
2019-02-02 16:43:12 +0200

    add valid to phone

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,7 @@ class Restaurant < ApplicationRecord
   before_save :create_token
 
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  # validates :phone_number => {format: /\d{1}-\d{3}-\d{3}-\d{3}/}
+  validates :phone_number, format: { with: /\d{1}-\d{3}-\d{3}-\d{3}/}
   validates :name, presence: true
 
   has_many :shifts
