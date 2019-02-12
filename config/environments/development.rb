ac8f6dd7903ba25568040a5e5bff593c98364bb2
Sypa T <taras.s@coaxsoft.com>
2019-02-12 16:32:43 +0200

    mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,11 +31,25 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.default_url_options = { host: 'localhost:3001', protocol: :http }
+  # config.action_mailer.default_url_options = { host: 'localhost:3001', protocol: :http }
+  config.action_mailer.default_url_options = { host: 'https://stark-hollows-58786.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+
+  # config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+
+
 
   config.action_mailer.perform_caching = false
 
